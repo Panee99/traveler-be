e8ea9d43eb4e87878289f27362d1162d39fd7a31
TRUONGNX\truon <truongnx2611@gmail.com>
2023-03-08 22:30:31 +0700

    implemented service Result wrapper

---
+++ Application/Controllers/ApiController.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using Application.Commons;
+using Microsoft.AspNetCore.Mvc;
+using Service.Results;
+
+namespace Application.Controllers;
+
+[ApiController]
+public class ApiController : ControllerBase
+{
+    private readonly ILogger<ApiController> _logger;
+
+    public ApiController(ILogger<ApiController> logger)
+    {
+        _logger = logger;
+    }
+
+    protected IActionResult OnError(Error error)
+    {
+        var response = new ObjectResult(new ErrorResponsePayload()
+        {
+            Timestamp = DateTime.Now,
+            Code = error.Code,
+            Message = error.Message,
+            Details = error.ErrorDetails
+        });
+
+        switch (error.ErrorType)
+        {
+            case ErrorType.Unexpected:
+                _logger.LogError("Unexpected error: {Code} - {Message}", error.Code, error.Message);
+                response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                break;
+            case ErrorType.Failure:
+                _logger.LogError("Failure error: {Code} - {Message}", error.Code, error.Message);
+                response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                break;
+            case ErrorType.Validation:
+                response.StatusCode = (int)HttpStatusCode.BadRequest;
+                break;
+            case ErrorType.Conflict:
+                response.StatusCode = (int)HttpStatusCode.Conflict;
+                break;
+            case ErrorType.NotFound:
+                response.StatusCode = (int)HttpStatusCode.NotFound;
+                break;
+        }
+        return response;
+    }
+}
---
+++ Service/Results/Result.cs
@@ -0,0 +1,24 @@
+﻿namespace Service.Results;
+
+public class Result
+{
+    public bool IsSuccess { get; }
+
+    public Error Error { get; }
+
+    protected static readonly Error NoError = new(ErrorType.Unexpected, "General.NoError",
+        "Success result has no error.");
+
+    protected Result(bool isSuccess, Error error)
+    {
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public static Result Success() => new(true, NoError);
+
+    public static implicit operator Result(Error error) => new(false, error);
+    
+    public TResult Match<TResult>(Func<TResult> onSuccess, Func<Error, TResult> onError) =>
+        IsSuccess ? onSuccess() : onError(Error);
+}
