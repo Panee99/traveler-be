c82a7914c16235c76e437bc93639d728d75726b5
TRUONGNX\truon <truongnx2611@gmail.com>
2023-03-12 22:13:27 +0700

    fix Serilog configuration

--- Application/Program.cs
+++ Application/Program.cs
@@ -4,42 +4,60 @@ using Data;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
+using Serilog;
 
+// Serilog for file logging
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Warning()
+    .WriteTo.File(path: "logs/log-.txt", rollingInterval: RollingInterval.Month,
+        outputTemplate: "[{Level:w3}] {Timestamp:dd-MM-yyyy HH:mm:ss.fff zzz}{NewLine}{Message:lj}{NewLine}{Exception}")
+    .CreateLogger();
+
+// BUILDER
 var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
-builder.AddSettings();
-builder.Services.AddDependenceInjection();
+{
+    // Add Serilog
+    builder.Logging
+        .ClearProviders()
+        .AddConsole()
+        .AddSerilog();
+    
+    // Add services to the container.
+    builder.AddSettings();
+    builder.Services.AddDependenceInjection();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGenNewtonsoftSupport();
-builder.Services.AddSwagger();
-builder.Services.AddCors();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddControllersWithViews()
-    .AddNewtonsoftJson(options =>
-        {
-            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-            options.SerializerSettings.Converters.Add(new StringEnumConverter());
-            options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-        }
-    );
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    builder.Services.AddControllers();
+    builder.Services.AddSwaggerGenNewtonsoftSupport();
+    builder.Services.AddSwagger();
+    builder.Services.AddCors();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddControllersWithViews()
+        .AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                options.SerializerSettings.Converters.Add(new StringEnumConverter());
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            }
+        );
+}
 
+// APPLICATION
 var app = builder.Build();
-Console.WriteLine(app.Environment.EnvironmentName);
-app.UseMiddleware<ErrorLoggingMiddleware>();
-
-// Configure the HTTP request pipeline.
-app.UseCors(x => x
-    .AllowAnyHeader()
-    .AllowAnyMethod()
-    .AllowAnyOrigin());
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseAuthentication();
-app.UseMiddleware<JwtMiddleware>();
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
+{
+    Console.WriteLine(app.Environment.EnvironmentName);
+    // Configure the HTTP request pipeline.
+    app.UseCors(x => x
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin());
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseAuthentication();
+    app.UseMiddleware<JwtMiddleware>();
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
