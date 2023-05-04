714da748effbdbfba6b0062d3bb74602f0ecf5a0
TRUONGNX\truon <truongnx2611@gmail.com>
2023-05-04 20:07:59 +0700

    update TourFilterViewModel

--- Service/Models/Tour/TourFilterViewModel.cs
+++ Service/Models/Tour/TourFilterViewModel.cs
@@ -5,12 +5,16 @@ namespace Service.Models.Tour;
 public class TourFilterViewModel
 {
     public Guid Id;
+    public string Code = null!;
+    public int MaxOccupancy;
     public string Title = null!;
     public string Departure = null!;
     public string Destination = null!;
     public DateTime StartTime;
     public DateTime EndTime;
     public double AdultPrice;
+    public double ChildrenPrice;
+    public double InfantPrice;
     public string? ThumbnailUrl;
     public string? Description;
     public TourType Type;
