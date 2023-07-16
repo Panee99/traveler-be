a7b9df2798bdca6bbcb429550788781b5f0c1b55
TRUONGNX\truon <truongnx2611@gmail.com>
2023-07-16 16:23:41 +0700

    fix tour group, trip model

--- Service/Models/TourGroup/TourGroupViewModel.cs
+++ Service/Models/TourGroup/TourGroupViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Service.Models.Trip;
+﻿using Data.Enums;
+using Service.Models.Trip;
 
 namespace Service.Models.TourGroup;
 
@@ -11,5 +12,6 @@ public class TourGroupViewModel
     public Guid TripId;
     public int TravelerCount;
     public Guid? CurrentScheduleId;
+    public TourGroupStatus Status;
     public TripViewModel? Trip;
 }
