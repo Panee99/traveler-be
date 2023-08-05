6a3dbcf2b11be2a04c5037b87064839159c25e99
TRUONGNX\truon <truongnx2611@gmail.com>
2023-08-05 23:29:39 +0700

    add trip details page

--- Application/Pages/Manager/Login.cshtml.cs
+++ Application/Pages/Manager/Login.cshtml.cs
@@ -3,32 +3,29 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Application.Pages.Manager;
 
-public class LoginModel : PageModel
+public class Login : PageModel
 {
-    [BindProperty]
-    public string Email { get; set; }
+    [BindProperty] public string Email { get; set; }
 
-    [BindProperty]
-    public string Password { get; set; }
+    [BindProperty] public string Password { get; set; }
 
-    [TempData]
-    public string? ErrorMessage { get; set; }
-    
+    [TempData] public string? ErrorMessage { get; set; }
+
     public IActionResult OnPost()
     {
         Console.WriteLine(Email);
         Console.WriteLine(Password);
-        
+
         if (Email == "truong@gmail.com" && Password == "123123")
         {
             return RedirectToPage("/index");
         }
 
         ErrorMessage = "Invalid email or password.";
-        
+
         return Page();
     }
-    
+
     public void OnGet()
     {
     }
---
+++ Application/Pages/Manager/TripDetails.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Data.EFCore;
+using Data.Entities;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Service.Interfaces;
+using Service.Models.Tour;
+
+namespace Application.Pages.Manager;
+
+public class TripDetails : PageModel
+{
+    private readonly UnitOfWork _unitOfWork;
+    private readonly ICloudStorageService _cloudStorageService;
+    private readonly ITourService _tourService;
+
+    public TripDetails(
+        UnitOfWork unitOfWork,
+        ICloudStorageService cloudStorageService,
+        ITourService tourService)
+    {
+        _unitOfWork = unitOfWork;
+        _cloudStorageService = cloudStorageService;
+        _tourService = tourService;
+    }
+
+    public Trip Trip { get; set; }
+    public TourDetailsViewModel Tour { get; set; }
+
+    public async Task OnGet(Guid id)
+    {
+        var trip = await _unitOfWork.Trips
+            .Query()
+            .Where(trip => trip.Id == id)
+            .Include(trip => trip.TourGroups).ThenInclude(group => group.Travelers)
+            .Include(trip => trip.TourGroups).ThenInclude(group => group.TourGuide)
+            .FirstOrDefaultAsync();
+
+        if (trip != null) Trip = trip;
+
+        var tourResult = await _tourService.GetDetails(Trip.TourId);
+        if (tourResult.IsSuccess) Tour = tourResult.Value;
+    }
+}
