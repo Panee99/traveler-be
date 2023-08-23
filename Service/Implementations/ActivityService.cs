49008bd687dd70364aef5a8699acb921f6e9d390
TRUONGNX\truon <truongnx2611@gmail.com>
2023-08-23 13:52:48 +0700

    minor fix

--- Service/Implementations/ActivityService.cs
+++ Service/Implementations/ActivityService.cs
@@ -82,8 +82,7 @@ public class ActivityService : BaseService, IActivityService
             if (tourGroup.TourGuideId != null) receiverIds.Add(tourGroup.TourGuideId.Value);
 
             await _notificationService.EnqueueNotification(
-                new NotificationJob(receiverIds, NotificationType.AttendanceActivity, null,
-                    dataModel?.Title));
+                new NotificationJob(receiverIds, NotificationType.AttendanceActivity, null));
         }
 
         // Return
